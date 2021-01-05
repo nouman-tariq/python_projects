b3161dc085534ecd29cc82aacc53b892d490a366
Muhammad Nouman Tariq <66955797+nouman-tariq@users.noreply.github.com>
2021-01-05 20:52:42 +0500

    starting the task again

--- main_file.py
+++ main_file.py
@@ -7,4 +7,12 @@ from selenium.webdriver.common.keys import Keys
 browser = webdriver.Firefox(executable_path="/usr/bin/geckodriver")
 browser.get('https://tanzeem.org/multimedia/magazines/')
 # print('Title: %s' % browser.title)
-# browser.quit()
+
+content = browser.page_source
+soup = BeautifulSoup(content, features='html.parser')
+# print(soup.prettify())
+
+for a in soup.findAll('div', attrs={'class': "col-sm-12 category-items all-items"}):
+    print(a)
+
+browser.quit()
