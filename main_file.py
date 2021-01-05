f875bda8e4a60c1458fe1e8fea5ea077c15118aa
Muhammad Nouman Tariq <66955797+nouman-tariq@users.noreply.github.com>
2021-01-06 00:21:06 +0500

    still trying to get the latest magazine -_-

--- main_file.py
+++ main_file.py
@@ -1,18 +1,8 @@
-from selenium import webdriver
 from bs4 import BeautifulSoup
-import pandas as pd
-from selenium.webdriver.common.keys import Keys
+import urllib.request
 
+html_page = urllib.request.urlopen("https://tanzeem.org/multimedia/magazines/nida-e-khilafat/")
+soup = BeautifulSoup(html_page, features='html.parser')
 
-browser = webdriver.Firefox(executable_path="/usr/bin/geckodriver")
-browser.get('https://tanzeem.org/multimedia/magazines/')
-# print('Title: %s' % browser.title)
-
-content = browser.page_source
-soup = BeautifulSoup(content, features='html.parser')
-# print(soup.prettify())
-
-for a in soup.findAll('div', attrs={'class': "col-sm-12 category-items all-items"}):
-    print(a)
-
-browser.quit()
+for a in soup.findAll('div', attrs={'class': 'category-list'}):
+    print(a.prettify)
