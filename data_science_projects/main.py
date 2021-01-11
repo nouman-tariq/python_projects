4a1a59b66af9122c43b62188118635a21d098c89
Muhammad Nouman Tariq <66955797+nouman-tariq@users.noreply.github.com>
2021-01-11 14:48:19 +0500

    adding the project

---
+++ data_science_projects/main.py
@@ -0,0 +1,13 @@
+# git clone https://github.com/bisguzar/twitter-scraper.git
+# cd twitter-scraper
+# sudo python3 setup.py install
+# pip3 install twitter_scraper
+
+
+# from twitter_scraper import get_tweets
+# for tweet in get_tweets('twitter', pages=3):
+#     print(tweet['text'])
+
+# # from twitter_scraper import get_trends
+# # get_trends()
+
--- machine_learning/main.py
+++
@@ -1,9 +0,0 @@
-from datapackage import Package
-
-package = Package('https://datahub.io/core/gdp/datapackage.json')
-
-print(package.resource_names)
-
-for resource in package.resources:
-    if resource.descriptor['datahub']['type'] == 'derived/csv':
-        print(resource.read())
