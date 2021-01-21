d67ff923e2349f9c8692764872570277b98b1a99
Muhammad Nouman Tariq <66955797+nouman-tariq@users.noreply.github.com>
2021-01-21 08:09:58 +0500

    cleaning the data

--- machine_learning/main.py
+++ machine_learning/main.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
 weather_data = pd.read_csv('weatherAUS.csv')
+test_data = weather_data[
+    (weather_data['Location'] == 'Sydney') & 
+    (weather_data['Date'] > '2015-01-01')]
 
 
 
-
-print(weather_data)
+print(test_data)
