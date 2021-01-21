f878c20ea57f4510c2319355faba7198186de918
Muhammad Nouman Tariq <66955797+nouman-tariq@users.noreply.github.com>
2021-01-21 08:30:49 +0500

    extracted the df

--- machine_learning/main.py
+++ machine_learning/main.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
 weather_data = pd.read_csv('weatherAUS.csv')
+
 test_data = weather_data[
     (weather_data['Location'] == 'Sydney') & 
     (weather_data['Date'] > '2015-01-01')]
+test_data = test_data.reset_index()
 
+cols = [1, 2, 5]
+df = test_data[test_data.columns[cols]]
 
 
 print(test_data)
